89eb4aa235adf643be99781e14c37bb122ea0f9c
Humber <ahfernandez-es@udabol.edu.bo>
2022-02-17 17:34:55 -0400

    First commit

---
+++ AwesomeFunction.js
@@ -0,0 +1,15 @@
+function add(number1, number2) {
+  pass;
+}
+
+function substract(number1, number2) {
+  pass;
+}
+
+function multiply(number1, number2) {
+  pass;
+}
+
+function divide(number1, number2) {
+  pass;
+}
